7b6da16553bf031aebb4a4575283c95bb1754d23
Werner Schröter <werner.schroeter@geosphere.de>
2022-01-24 19:29:36 +0100

    InitialCommit for Branch

---
+++ app/src/main/java/com/example/newhomeworkbook/calender/CalendarDayItemView.java
@@ -0,0 +1,39 @@
+package com.example.newhomeworkbook.calender;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.example.newhomeworkbook.databinding.CalenderDayViewBinding;
+
+public class CalendarDayItemView extends View {
+    public CalendarDayItemView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public CalendarDayItemView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public CalendarDayItemView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    public CalendarDayItemView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    private void init(Context context){
+        CalenderDayViewBinding viewBinding = CalenderDayViewBinding.inflate(LayoutInflater.from(context));
+        ConstraintLayout constraintLayout = viewBinding.getRoot();
+
+    }
+}
