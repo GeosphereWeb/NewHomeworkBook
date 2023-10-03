1f647ba9115fe45df2c655b55ebe6cc1f325a791
Werner Schröter <werner.schroeter@geosphere.de>
2023-10-03 17:09:55 +0200

    ktlint added

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,33 @@
 plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.0"
 }
 true // Needed to make the Suppress annotation work for the plugins block
+
+buildscript {
+    repositories {
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
+    }
+    dependencies {
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:11.6.0")
+    }
+}
+
+apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint") // Version should be inherited from parent
+
+    repositories {
+        // Required to download KtLint
+        mavenCentral()
+    }
+
+    // Optionally configure plugin
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        debug.set(true)
+    }
+}
