c77047595d35ff112f890bfc3b473a9acf812082
Werner Schröter <werner.schroeter@geosphere.de>
2023-12-08 18:30:04 +0100

    Update to Gradle Plugin to 8.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.com.android.application)
-    alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import io.gitlab.arturbosch.detekt.Detekt
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.com.android.application) apply false
-    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.6.0"
     id("io.gitlab.arturbosch.detekt") version("1.23.3")
 }
